8ffb036d090cd871a05709f8e38a04fa484dc94b
carlosprado0 <ibroonkzz@gmail.com>
2025-11-20 20:24:42 -0300

    db e pastas

---
+++ services/web/index.ts
@@ -0,0 +1,4 @@
+export { default as ProductService } from "services\web\orderService";
+export { default as CategoryService } from "services\web\categoryService";
+export { default as OrderService } from "services\web\orderService";
+export { default as storage } from "services\web\storage";
